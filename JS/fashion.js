b0b8caeea8155f977d741e1ae50dbc2454ebb4f0
ruthjomo <ruthjomo19@gmail.com>
2020-08-05 15:06:58 +0300

    corrected some codes

--- JS/fashion.js
+++ JS/fashion.js
@@ -11,11 +11,57 @@ $(document).ready(function(){
         $("#blue").slideUp(3000);
         $("#blue").slideDown(3000);
     })
-    $(".click").click(function(){
-        $("#history").toggle();
-        $("#click").toggle();
+});
+$(document).ready(function(){
+    $(".pic1").click(function(){
+        $("#pic1-p").toggle(3000);
+        $("#pic1").toggle(3000);
     })
-    
+    $(".pic2").click(function(){
+        $("#pic2-p").toggle(3000);
+        $("#pic2").toggle(3000);
+    })
+    $(".pic3").click(function(){
+        $("#pic3-p").toggle(3000);
+        $("#pic3").toggle(3000);
+    })
+    $(".pic4").click(function(){
+        $("#pic4-p").toggle(3000);
+        $("#pic4").toggle(3000);
+    })
+    $(".pic5").click(function(){
+        $("#pic5-p").toggle(3000);
+        $("#pic5").toggle(3000);
+    })
+    $(".pic6").click(function(){
+        $("#pic6-p").toggle(3000);
+        $("#pic6").toggle(3000);
+    })
+    $(".pic7").click(function(){
+        $("#pic7-p").toggle(3000);
+        $("#pic7").toggle(3000);
+    })
+    $(".pic8").click(function(){
+        $("#pic8-p").toggle(3000);
+        $("#pic8").toggle(3000);
+    })
+    $(".pic9").click(function(){
+        $("#pic9-p").toggle(3000);
+        $("#pic9").toggle(3000);
+    })
+    $(".pic10").click(function(){
+        $("#pic10-p").toggle(3000);
+        $("#pic10").toggle(3000);
+    })
+    $(".pic11").click(function(){
+        $("#pic11-p").toggle(3000);
+        $("#pic11").toggle(3000);
+    })
+    $(".pic12").click(function(){
+        $("#pic12-p").toggle(3000);
+        $("#pic12").toggle(3000);
+    })
+
 });
 
 var slideIndex = 0;
@@ -46,5 +92,29 @@ $(document).ready(function(){
     $(".slider").click(function(){
         $("#slide3-p").toggle(3000);
         $("#slide3").toggle(3000);
+    })
+    $(".men1").click(function(){
+        $("#men1-p").toggle(3000);
+        $("#men1").toggle(3000);
+    })
+    $(".men2").click(function(){
+        $("#men2-p").toggle(3000);
+        $("#men2").toggle(3000);
+    })
+    $(".men3").click(function(){
+        $("#men3-p").toggle(3000);
+        $("#men3").toggle(3000);
+    })
+    $(".kid1").click(function(){
+        $("#kid1-p").toggle(3000);
+        $("#kid1").toggle(3000);
+    })
+    $(".kid2").click(function(){
+        $("#kid2-p").toggle(3000);
+        $("#kid2").toggle(3000);
+    })
+    $(".kid3").click(function(){
+        $("#kid3-p").toggle(3000);
+        $("#kid3").toggle(3000);
     })
 });             
