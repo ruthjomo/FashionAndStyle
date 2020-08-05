5896e3e36d34e09a852c148cc6fbf26f6b32a129
ruthjomo <ruthjomo19@gmail.com>
2020-08-05 09:27:21 +0300

    added more images and codes

--- JS/fashion.js
+++ JS/fashion.js
@@ -1,7 +1,22 @@
-$(documeent).ready(function(){
-    $("#image").click(function(){
-        $("#history").toggle();
-        $("#image").toggle();
-        
+$(document).ready(function(){
+    $("#move1").hover(function(){
+        $("#move1").slideUp(3000);
+        $("#move1").slideDown(3000);
     })
+    $("#move2").hover(function(){
+        $("#move2").slideUp(3000);
+        $("#move2").slideDown(3000);
+    })
+    $("#blue").hover(function(){
+        $("#blue").slideUp(3000);
+        $("#blue").slideDown(3000);
+    })
+    $(".click").click(function(){
+        $("#history").toggle();
+        $("#click").toggle();
+    })
+    // $(".clicke").click(function(){
+    //     $("#history").toggle();
+    //     $("#image").toggle();
+    // })
 });
