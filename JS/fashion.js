439b52e1a181ae7b43ffdd70103a9b43cd7550fa
ruthjomo <ruthjomo19@gmail.com>
2020-08-05 11:06:12 +0300

    Added more images and codes

--- JS/fashion.js
+++ JS/fashion.js
@@ -15,8 +15,22 @@ $(document).ready(function(){
         $("#history").toggle();
         $("#click").toggle();
     })
-    // $(".clicke").click(function(){
-    //     $("#history").toggle();
-    //     $("#image").toggle();
-    // })
+    
 });
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+var i;
+var slides = document.getElementsByClassName("slides");
+for (i = 0; i < slides.length; i++) {
+slides[i].style.display = "none";
+}
+slideIndex++;
+if (slideIndex > slides.length) {slideIndex = 1}
+slides[slideIndex-1].style.display = "block";
+setTimeout(showSlides, 2000); 
+}
+
+                 
